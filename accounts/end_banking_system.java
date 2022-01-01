0bde20cb87f5a37fb906b47d13e00f7ff53a1a7d
M Mahi Pavan Teja Sathguna Sai <59690019+mahi101101@users.noreply.github.com>
2022-01-02 02:06:59 +0530

    updated for new class usage

--- accounts/end_banking_system.java
+++ accounts/end_banking_system.java
@@ -3,42 +3,93 @@ package exam;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class end_banking_system implements sender,benificiary{
-	String IFSC;
-	String IFSC1;
-	String sender_accountNum;
-	String sender_accountDetails;
-	String benificiary_accountNum;
-	String benificiary_accountDetails;
-	public end_banking_system() {
-		this.IFSC="IDIB10000001";
-		this.IFSC1="IDIB10000002";
-		this.benificiary_accountDetails="Indian Bank - Vellore";
-		this.benificiary_accountNum="33345663453";
-		this.sender_accountNum="53545963453";
-		this.sender_accountDetails="ICICI Bank - Chennai";
+public class end_banking_system  {
+	boolean transSuccess;
+	boolean sufficientFunds;
+	
+	end_banking_system(){
+		this.sufficientFunds=false;
+		this.transSuccess=false;
 	}
-	public String getIFSC() {return this.IFSC;}
-	public String getIFSC1() {return this.IFSC1;}
-	public String getsender_accountNum(){return this.sender_accountNum;}
-	public String getsender_accountDetails(){return this.sender_accountDetails;}
-	public String getbenificiary_accountNum(){return this.benificiary_accountNum;}
-	public String getbenificiary_accountDetails(){return this.benificiary_accountDetails;}
 	
-	public void setIFSC(String IFCS){this.IFSC=IFSC;}
-	public void setIFSC1(String IFCS){this.IFSC1=IFSC;}
-	public void setsender_accountNum(String accNum){this.sender_accountNum=accNum;}
-	public void setsender_accountDetails(String accDetails){this.sender_accountDetails=accDetails;}
-	public void setbenificiary_accountNum(String accNum){this.benificiary_accountNum=accNum;}
-	public void setbenificiary_accountDetails(String accDetails){this.benificiary_accountDetails=accDetails;}
+	public void setSufficientFunds(boolean status) {this.sufficientFunds=status;}
+	public void setTransSuccess(boolean status) {this.transSuccess=status;}
 	
-	public void sender_accountNumber() {}
-	public void sender_accountDetails() {}
-	public void benificiary_accountNumber() {}
-	public void benificiary_accountDetails() {}
-	public void IFSC_code() {}
-	public void bankAccount() {}
-	public void onlinePortal() {}
-	public void authorities() {}
+	public boolean getSufficientFunds() {return this.sufficientFunds;}
+	public boolean getTransSuccess() {return this.transSuccess;}
+	
+	public boolean checkIFSC( String verify, accounts reciever) {
+		
+		if(reciever.IFSC.equals(verify)) {return true;}
+		else {return false;}
+	}
+	
+	public void amountToTransfer(accounts sender) {
+		Scanner s = new Scanner(System.in);
+		float x=(float)1000.0;
+		System.out.print("Enter money to transfer : 1000.0 ");
+		
+		System.out.print("\n");
+		
+		sender.setTransAmount(x);
+		s.close();
+		
+	}
+	
+	public void transfer(accounts sender, accounts reciever) {
+		sender.setBalance(sender.balance-sender.transAmount);
+		reciever.setTransAmount(sender.transAmount);
+		reciever.setBalance(reciever.balance+reciever.transAmount);
+	}
+	
+	public void moneytransfer(accounts sender,accounts reciever) {
+		boolean status = getTransSuccess();
+		boolean funds = getSufficientFunds();
+		
+		funds =  reciever.checkBalanceForMinBal(sender.balance);
+		
+		if(funds) {
+			System.out.print("Suffient funds available\n");
+			amountToTransfer(sender);
+			transfer(sender,reciever);
+			status=true;
+		}
+		else{
+			status=false;
+		}
+		
+		if(status) {
+			System.out.println("Transaction succesful");
+		}
+		else {
+			System.out.println("Transaction terminated");
+		}
+		setSufficientFunds(funds);
+		setTransSuccess(status);
 
+		
+	}
+	
+	public void bankTransfer(accounts sender, accounts reciever) {
+		 String IFSC;
+		 Scanner sc = new Scanner(System.in);
+		 
+		 System.out.print("Enter the IFSC of benificiary : ");
+		 IFSC=sc.next();
+		 
+		 System.out.print("\n");
+		 
+		 if(checkIFSC(IFSC,reciever)) {
+			 sender.getBenificiary();
+			 moneytransfer(reciever,sender);
+			 sc.close();
+		 }
+		 else {
+			 System.out.println("The IFSC of benificiary not matched");
+			 sc.close();
+		 }
+		
+		 
+	}
+	
 }
